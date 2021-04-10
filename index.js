e943dc4adaa2ad5406ac8f2f9052f9114e941b39
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-04-10 14:53:13 -0300

    feat: add route to get movies by id

--- index.js
+++ index.js
@@ -25,8 +25,25 @@ var db = {
 };
 
 app.get('/movies', (req, res) => {
+  // Sempre retornar o status code da requisição
   res.statusCode = 200;
   res.json(db.movies);
+});
+
+app.get('/movies/:id', (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    // Percorre o array e encontra o filme que possui esse id
+    var movie = db.movies.find((movie) => movie.id == id);
+    if (movie != undefined) {
+      res.json(movie);
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  }
 });
 
 app.listen(5500, () => {
