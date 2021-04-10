b646d9c167c67df821056bf8315369583ea8c9db
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-04-10 15:15:45 -0300

    feat: add route to delete movies using delete method

--- index.js
+++ index.js
@@ -59,6 +59,24 @@ app.post('/movie', (req, res) => {
   }
 });
 
+app.delete('/movie/:id', (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    // Percorre o array e encontra o id do filme que seja igual ao parametro
+    var index = db.movies.findIndex((movie) => movie.id == id);
+    // Se findIndex retornar -1 quer dizer que não existe um filme correspondente
+    if (index == -1) {
+      res.sendStatus(404);
+    } else {
+      // Remove 1 elemento a partir do index encontrado
+      db.movies.splice(index, 1);
+      res.json(db);
+    }
+  }
+});
+
 app.listen(5500, () => {
   console.log('Api rodando');
 });
