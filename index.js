9f5656dc5a1d8fe67ae489bcd95fb347325860f2
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-04-10 14:41:19 -0300

    chore(dependencies): add and configure basic dependencies

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Fake database to avoid the work of conguring one
+var db = {
+  movies: [
+    {
+      id: 1,
+      title: 'Joker',
+    },
+    {
+      id: 2,
+      title: 'Batman: The Dark Knight',
+    },
+    {
+      id: 3,
+      title: 'Limitless',
+    },
+  ],
+};
+
+app.get('/movies', (req, res) => {
+  res.statusCode = 200;
+  res.json(db.movies);
+});
+
+app.listen(5500, () => {
+  console.log('Api rodando');
+});
